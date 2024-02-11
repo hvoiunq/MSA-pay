d4e0374358a3e4189b871a6b88e3ffcdb1237dac
hvoiunq <hyojunghan1016@gmail.com>
2024-02-11 11:43:27 +0900

    fix : firmbanking request 수정

--- banking-service/src/main/java/com/fastcampuspay/banking/adapter/in/web/dto/request/RequestFirmBankingRequest.java
+++ banking-service/src/main/java/com/fastcampuspay/banking/adapter/in/web/dto/request/RequestFirmBankingRequest.java
@@ -1,21 +1,18 @@
 package com.fastcampuspay.banking.adapter.in.web.dto.request;
 
 import com.fastcampuspay.banking.application.port.in.RequestFirmBankingCommand;
-import lombok.Builder;
-import lombok.Data;
 
-@Builder
-public class RequestFirmBankingRequest {
+public record RequestFirmBankingRequest (
     // a -> b 실물계좌 요청 Request
 
-    private String fromBankName;
-    private String fromBankAccountNumber;
-    private String toBankName;
-    private String toBankAccountNumber;
-    private int moneyAccount;
-
+    String fromBankName,
+    String fromBankAccountNumber,
+    String toBankName,
+    String toBankAccountNumber,
+    int moneyAccount
+) {
     public RequestFirmBankingCommand toCommand(RequestFirmBankingRequest request) {
-        new RequestFirmBankingCommand(
+        return new RequestFirmBankingCommand(
                 request.fromBankName,
                 request.fromBankAccountNumber,
                 request.toBankName,
